03ab9332c0e107cb2df5533de17f9fb48517b118
Mikeyudex <miguel.yudex@gmail.com>
2025-04-29 16:38:34 -0500

    se agrega deploy.log

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+FILE_LOG="deploy.log"
+
+# clear log
+echo "" > $FILE_LOG
+
 # install dependencies
-echo "Installing dependencies..."
+echo "Installing dependencies..." > $FILE_LOG
 npm install --force --silent
 
 # build
-echo "Building..."
+echo "Building..." > $FILE_LOG
 npm run build
 
 # deploy
-echo "Deploying..."
+echo "Deploying..." > $FILE_LOG
 npm run deploy
 
 # success
-echo "Deployment successful!"
+echo "Deployment successful!" > $FILE_LOG

64374e8bf7a75eaf4c735f0d83301438930eb03f
Mikeyudex <miguel.yudex@gmail.com>
2025-05-01 09:10:17 -0500

    se corrige deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,19 @@
 FILE_LOG="deploy.log"
 
 # clear log
-echo "" > $FILE_LOG
+echo "" >> $FILE_LOG
 
 # install dependencies
-echo "Installing dependencies..." > $FILE_LOG
+echo "Installing dependencies..." >> $FILE_LOG
 npm install --force --silent
 
 # build
-echo "Building..." > $FILE_LOG
+echo "Building..." >> $FILE_LOG
 npm run build
 
 # deploy
-echo "Deploying..." > $FILE_LOG
+echo "Deploying..." >> $FILE_LOG
 npm run deploy
 
 # success
-echo "Deployment successful!" > $FILE_LOG
+echo "Deployment successful!" >> $FILE_LOG

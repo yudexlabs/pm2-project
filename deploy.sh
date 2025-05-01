ae66378b7bec837a1da7a4f2a22eab710c4e245e
Mikeyudex <miguel.yudex@gmail.com>
2025-05-01 11:29:13 -0500

    se agrega deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,11 @@ npm run build
 
 # deploy
 echo "Deploying..." >> $FILE_LOG
-npm run deploy
+if pm2 list | grep -q 'online'; then #verifica si hay un servicio corriendo
+    pm2 reload ecosystem.config.js --env production
+else
+    npm run deploy
+fi
 
 # success
 echo "Deployment successful!" >> $FILE_LOG
